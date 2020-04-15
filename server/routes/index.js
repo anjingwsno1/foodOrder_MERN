fb482fa06946ed185f6965a7023894bbb354026a
anjingwsno1 <584381148@qq.com>
2020-04-15 16:46:12 +0800

    test backend connect frontend

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,16 @@
 const express = require("express");
 
 const CakeCtrl = require("../controllers/cake_controller.js");
+const MovieCtrl = require("../controllers/movie_controller.js");
 
 const router = express.Router();
 
 router.get("/cakes", CakeCtrl.getCakes);
+
+router.post("/movie", MovieCtrl.createMovie);
+router.put("/movie/:id", MovieCtrl.updateMovie);
+router.delete("/movie/:id", MovieCtrl.deleteMovie);
+router.get("/movie/:id", MovieCtrl.getMovieById);
+router.get("/movies", MovieCtrl.getMovies);
 
 module.exports = router;
